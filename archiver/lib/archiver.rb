6dd075a3a50f0450e9e2f01ac4c8cbd50de6659a
Leianne Louis <leilouis112@gmail.com>
2019-08-15 16:18:29 -0400

    gem  connected with rails app

--- archiver/lib/archiver.rb
+++ archiver/lib/archiver.rb
@@ -1,6 +1,16 @@
 require "archiver/version"
 
 module Archiver
-  class Error < StandardError; end
-  # Your code goes here...
+  
+  def archive
+    update(archived_at: Time.now)
+  end
+
+  def restore
+    update(archived_at: nil)
+  end
+
+  def archived?
+    archived_at.nil?
+  end
 end
